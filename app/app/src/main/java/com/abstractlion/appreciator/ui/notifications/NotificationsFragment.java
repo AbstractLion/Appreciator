1f9aa8efa113b6e6dbac4584668c376fb3df3982
naomi-rc <naomi.cat182128@gmail.com>
2020-01-26 10:23:10 -0500

    fixed layouts

--- app/app/src/main/java/com/abstractlion/appreciator/ui/notifications/NotificationsFragment.java
+++ app/app/src/main/java/com/abstractlion/appreciator/ui/notifications/NotificationsFragment.java
@@ -1,5 +1,6 @@
 package com.abstractlion.appreciator.ui.notifications;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -30,6 +32,7 @@ public class NotificationsFragment extends Fragment {
             public void onChanged(@Nullable String s) {
                 String rank = Integer.toString(AppreciateTracker.getRanking());
                 myRank.setText(rank);
+
             }
         });
         return root;
